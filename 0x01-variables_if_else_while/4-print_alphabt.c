fe4461766614a5dfe262fe8ced685a7a96a1b474
EG-Tokyoghoul <130866746+EG-Tokyoghoul@users.noreply.github.com>
2023-06-17 01:16:36 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/* main - Prints the alphabet in lowercase, except for 
-* q and e.
-*/
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char letter;
