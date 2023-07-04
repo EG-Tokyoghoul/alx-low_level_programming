c4ffd7a1fb27e6701dcdaeab90bf92f84457fb20
EG-Tokyoghoul <130866746+EG-Tokyoghoul@users.noreply.github.com>
2023-07-04 09:24:45 +0300

    Create 100-print_comb3.c
    remark

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+
+int main() {
+    int a=0;
+   while (a<10)
+   {
+     int b=1;
+       while (b<10)
+       {
+     if (a!=b && b>a)
+       {
+           putchar(48+a);
+           putchar(48+b);
+           if (a<8){
+           putchar(',');
+           }
+       }
+       b++;
+       
+       }   
+   a++;
+   }
+    return 0;
+}
