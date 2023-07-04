e9884b8be0be4e8594d71fc06f368bec0a1f2617
EG-Tokyoghoul <130866746+EG-Tokyoghoul@users.noreply.github.com>
2023-07-04 10:19:15 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void) {
+    int a=0;
+   while (a<10)
+   {
+     int b=1;
+       while (b<10)
+       {
+           int c=2;
+           while(c<10){
+        if (b!=c!=a && b>a && c>b)
+       {
+           putchar(48+a);
+           putchar(48+b);
+           putchar(48+c);
+           if (a<7){
+           putchar(',');
+            putchar(' ');
+            }
+           }
+           c++;
+       }
+       b++;
+       }   
+   a++;
+   }
+    return 0;
+}
