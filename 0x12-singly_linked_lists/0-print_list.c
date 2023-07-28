53edeff1f72587350bac412cda4846d51c15a9e7
EG-Tokyoghoul <130866746+EG-Tokyoghoul@users.noreply.github.com>
2023-07-28 12:34:31 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
  *
  * then it Return: the number of nodes print
  */
-size_t print_list(const list_t *l)
+ize_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (l)
+	while (h)
 	{
-		if (!l->str)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", l->len, l->str);
-		l = l->next;
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		s++;
 	}
 
